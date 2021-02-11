2e93e34337ee79745fc14044ae1ac00eb4b66e11
Marie Burki <marie.buerki@gmail.com>
2021-02-11 11:38:43 +0100

    implémenter entrées du formulaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -31,5 +31,24 @@ closeBtn.addEventListener("click", closeModal=>{
 }) ;
 
 // Validité du formulaire :
-// step 1 : définir les labels dans des constantee
-// écouter la validation du formulaire + fonction d'erreur + cas à ne pas valider
+// step 1 : définir la fonction validate (présente dans HTML)
+// step 2 : dans fonction, demander champs à être remplis (nom, prénom, mail, nombre de tournois)
+// step 3 : dans fonction, demander au moins un boutton radio à être selectionné
+// step 4 : dans fonction, demander checkbox conditions d'utilisation à être selectioné
+// note : pas besoin d'autres demandes (nombre de lettres etc.), elles sont inscrites dans le HTML (sauf pour "last", à ajouter)
+// note : les données du formulaire sont conservées par défaut tant qu'il n'est pas valide
+
+function validate() {
+  if (first.value=="" || last.value=="" || email.value=="" || quantity.value=="") {
+    alert ("Tous les champs doivent être remplis")
+    return false ;
+  };
+  if (!location1.checked && !location2.checked && !location3.checked && !location4.checked && !location5.checked && !location6.checked) {
+    alert ("Merci de sélectionner une ville")
+    return false;
+  };
+  if (!checkbox1.checked) {
+    alert ("Veuillez accepter les conditions d'utilisation")
+    return false;
+  };
+};
